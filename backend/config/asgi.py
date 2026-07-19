e3aecdd51207daf12a01271d727e546fe086086e
Divyan-SS <divyan9629@gmail.com>
2026-07-20 01:41:46 +0530

    Fix Django ASGI initialization order in config/asgi.py to resolve ImproperlyConfigured AUTH_USER_MODEL error on Railway

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,10 +1,13 @@
 import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import apps.telemetry.routing
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 django_asgi_app = get_asgi_application()
 
